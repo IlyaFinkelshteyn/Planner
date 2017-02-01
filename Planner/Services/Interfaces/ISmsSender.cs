09403c452a708e63f6a6fc11086adabff80effce
Tony Richards <trichards57@gmail.com>
2017-02-01 17:30:43 +0000

    Minor tidy up of interfaces.

---
+++ Planner/Services/Interfaces/ISmsSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Planner.Services.Interfaces
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
--- Planner/Services/MessageServices.cs
+++ Planner/Services/MessageServices.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Planner.Services.Interfaces;
 using System.Threading.Tasks;
 
 namespace Planner.Services
