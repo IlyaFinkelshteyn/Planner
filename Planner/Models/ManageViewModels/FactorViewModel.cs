c551daccfa3a0e3b9834994d05a43c66d4034a00
Tony Richards <trichards57@gmail.com>
2017-02-01 17:28:25 +0000

    Initial Project Commit

---
+++ Planner/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Planner.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
