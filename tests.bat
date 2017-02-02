c3b5ec94b747b499f96aab2585099f360acb36f5
Tony Richards <trichards57@gmail.com>
2017-02-02 18:02:07 +0000

    Updated test script.

--- tests.bat
+++ tests.bat
@@ -3,10 +3,10 @@ mkdir reports\coverage\history
 
 nuget install OpenCover -ExcludeVersion -OutputDirectory  tools
 nuget install xunit.runner.console -ExcludeVersion -OutputDirectory tools -Pre
-nuget install coveralls.io -ExcludeVersion -OutputDirectory tools
-nuget install ReportGenerator -ExcludeVersion -OutputDirectory tools
+if "%APPVEYOR%"=="True" nuget install coveralls.io -ExcludeVersion -OutputDirectory tools
+if "%APPVEYOR%"=="" nuget install ReportGenerator -ExcludeVersion -OutputDirectory tools
 
-dotnet build
+if "%APPVEYOR%"=="" dotnet build
 
 tools\OpenCover\tools\OpenCover.Console.exe -target:"tools\xunit.runner.console\tools\xunit.console.x86.exe" -targetargs:"Planner.Tests\bin\Debug\net452\Planner.Tests.exe -noShadow -appveyor" -register:user -output:"reports\coverage\coverage.xml" -skipautoprops -filter:"+[Planner*]* -[Planner.Test*]*"  -excludebyattribute:ExcludeFromCodeCoverage -mergebyhash
 
