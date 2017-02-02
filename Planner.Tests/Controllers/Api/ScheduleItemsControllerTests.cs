a62f7ce0f2de0a9897cdf6ca15c8d708a0f3c45c
Tony Richards <trichards57@gmail.com>
2017-02-02 21:54:46 +0000

    Implemented ScheduleItemsController

---
+++ Planner.Tests/Controllers/Api/ScheduleItemsControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Planner.Controllers.Api;
+using Planner.Models.EventsModel;
+using Planner.Models.EventsViewModels;
+using Planner.Services.Interfaces;
+using Ploeh.AutoFixture;
+
+namespace Planner.Tests.Controllers.Api
+{
+    public class ScheduleItemsControllerTests : SubItemControllerTestsBase<ScheduleItemsController, ScheduleItem, ScheduleItemCreate, ScheduleItemDetails>
+    {
+        protected override ScheduleItemsController GetController(ISubItemService<ScheduleItem> service)
+        {
+            return new ScheduleItemsController(service);
+        }
+
+        protected override JsonPatchDocument<ScheduleItem> GetPatch()
+        {
+            var patch = new JsonPatchDocument<ScheduleItem>();
+
+            var newAction = Fixture.Create<string>();
+
+            patch.Replace(d => d.Action, newAction);
+
+            return patch;
+        }
+    }
+}
---
+++ Planner/Controllers/Api/ScheduleItemsController.cs
@@ -0,0 +1,13 @@
+﻿using Planner.Models.EventsModel;
+using Planner.Models.EventsViewModels;
+using Planner.Services.Interfaces;
+
+namespace Planner.Controllers.Api
+{
+    public class ScheduleItemsController : SubItemControllerBase<ScheduleItem, ScheduleItemCreate, ScheduleItemDetails>
+    {
+        public ScheduleItemsController(ISubItemService<ScheduleItem> service) : base(service)
+        {
+        }
+    }
+}
