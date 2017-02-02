7e3812c0b82732b1141a0b1693ad378d90f6baab
Tony Richards <trichards57@gmail.com>
2017-02-02 18:14:11 +0000

    Minor change to appveyor to use local xunit console

--- tests.bat
+++ tests.bat
@@ -2,13 +2,14 @@ mkdir tools
 mkdir reports\coverage\history
 
 nuget install OpenCover -ExcludeVersion -OutputDirectory  tools
-nuget install xunit.runner.console -ExcludeVersion -OutputDirectory tools -Pre
+if "%APPVEYOR%"=="" nuget install xunit.runner.console -ExcludeVersion -OutputDirectory tools
 if "%APPVEYOR%"=="True" nuget install coveralls.io -ExcludeVersion -OutputDirectory tools
 if "%APPVEYOR%"=="" nuget install ReportGenerator -ExcludeVersion -OutputDirectory tools
 
 if "%APPVEYOR%"=="" dotnet build
 
-tools\OpenCover\tools\OpenCover.Console.exe -target:"tools\xunit.runner.console\tools\xunit.console.x86.exe" -targetargs:"Planner.Tests\bin\Debug\net452\Planner.Tests.exe -noShadow -appveyor" -register:user -output:"reports\coverage\coverage.xml" -skipautoprops -filter:"+[Planner*]* -[Planner.Test*]*"  -excludebyattribute:ExcludeFromCodeCoverage -mergebyhash
+if "%APPVEYOR%"=="" tools\OpenCover\tools\OpenCover.Console.exe -target:"tools\xunit.runner.console\tools\xunit.console.x86.exe" -targetargs:"Planner.Tests\bin\Debug\net452\Planner.Tests.exe -noShadow -appveyor" -register:user -output:"reports\coverage\coverage.xml" -skipautoprops -filter:"+[Planner*]* -[Planner.Test*]*"  -excludebyattribute:ExcludeFromCodeCoverage -mergebyhash
+if "%APPVEYOR%"=="True" tools\OpenCover\tools\OpenCover.Console.exe -target:"xunit.console" -targetargs:"Planner.Tests\bin\Debug\net452\Planner.Tests.exe -noShadow -appveyor" -register:user -output:"reports\coverage\coverage.xml" -skipautoprops -filter:"+[Planner*]* -[Planner.Test*]*"  -excludebyattribute:ExcludeFromCodeCoverage -mergebyhash
 
 if "%APPVEYOR%"=="True" tools\coveralls.io\tools\coveralls.net.exe --opencover reports\coverage\coverage.xml
 
