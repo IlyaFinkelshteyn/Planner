3183782cc5aaa33048a248de4678321004760675
Tony Richards <trichards57@gmail.com>
2017-02-02 21:39:36 +0000

    Implemented NoGoAreasController.

---
+++ Planner.Tests/Controllers/Api/NoGoAreasControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Planner.Controllers.Api;
+using Planner.Models.EventsModel;
+using Planner.Models.EventsViewModels;
+using Planner.Services.Interfaces;
+using Ploeh.AutoFixture;
+
+namespace Planner.Tests.Controllers.Api
+{
+    public class NoGoAreasControllerTests : SubItemControllerTestsBase<NoGoAreasController, NoGoArea, NoGoAreaCreate, NoGoAreaDetails>
+    {
+        protected override NoGoAreasController GetController(ISubItemService<NoGoArea> service)
+        {
+            return new NoGoAreasController(service);
+        }
+
+        protected override JsonPatchDocument<NoGoArea> GetPatch()
+        {
+            var patch = new JsonPatchDocument<NoGoArea>();
+
+            var newName = Fixture.Create<string>();
+
+            patch.Replace(d => d.Name, newName);
+
+            return patch;
+        }
+    }
+}
---
+++ Planner/Controllers/Api/NoGoAreasController.cs
@@ -0,0 +1,13 @@
+﻿using Planner.Models.EventsModel;
+using Planner.Models.EventsViewModels;
+using Planner.Services.Interfaces;
+
+namespace Planner.Controllers.Api
+{
+    public class NoGoAreasController : SubItemControllerBase<NoGoArea, NoGoAreaCreate, NoGoAreaDetails>
+    {
+        public NoGoAreasController(ISubItemService<NoGoArea> service) : base(service)
+        {
+        }
+    }
+}
