615051dc9de9500a70d0b3a8ae60a9f0b66a6fab
Tony Richards <trichards57@gmail.com>
2017-02-25 11:31:26 +0000

    Added uploading test results to appveyor.

--- tests.bat
+++ tests.bat
@@ -8,8 +8,10 @@ if "%APPVEYOR%"=="" nuget install ReportGenerator -ExcludeVersion -OutputDirecto
 
 if "%APPVEYOR%"=="" dotnet build
 
-tools\OpenCover\tools\OpenCover.Console.exe -target:"tools\xunit.runner.console\tools\xunit.console.x86.exe" -targetargs:"Planner.Tests\bin\Debug\net462\Planner.Tests.exe -noShadow -appveyor" -register:user -output:"reports\coverage\coverage.xml" -skipautoprops -filter:"+[Planner*]* -[Planner*]Planner.Data.Migrations* -[Planner.Test*]*"  -excludebyattribute:*.ExcludeFromCodeCoverage* -mergebyhash
+tools\OpenCover\tools\OpenCover.Console.exe -target:"tools\xunit.runner.console\tools\xunit.console.x86.exe" -targetargs:"Planner.Tests\bin\Debug\net462\Planner.Tests.exe -noShadow -xml test-results.xml" -register:user -output:"reports\coverage\coverage.xml" -skipautoprops -filter:"+[Planner*]* -[Planner*]Planner.Data.Migrations* -[Planner.Test*]*"  -excludebyattribute:*.ExcludeFromCodeCoverage* -mergebyhash
 
 if "%APPVEYOR%"=="True" tools\coveralls.io\tools\coveralls.net.exe --opencover reports\coverage\coverage.xml
 
 if "%APPVEYOR%"=="" tools\ReportGenerator\tools\ReportGenerator.exe -reports:reports\coverage\coverage.xml -targetdir:reports\coverage -historydir:reports\coverage\history
+
+if "%APPVEYOR%"=="True" powershell .\upload-results.ps1
