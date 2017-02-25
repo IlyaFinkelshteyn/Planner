6b12832fae0c1e52b27b84228511a8a92937201d
Tony Richards <trichards57@gmail.com>
2017-02-25 11:12:26 +0000

    Updated tests to reflect current .net version.

--- tests.bat
+++ tests.bat
@@ -8,7 +8,7 @@ if "%APPVEYOR%"=="" nuget install ReportGenerator -ExcludeVersion -OutputDirecto
 
 if "%APPVEYOR%"=="" dotnet build
 
-tools\OpenCover\tools\OpenCover.Console.exe -target:"tools\xunit.runner.console\tools\xunit.console.x86.exe" -targetargs:"Planner.Tests\bin\Debug\net452\Planner.Tests.exe -noShadow -appveyor" -register:user -output:"reports\coverage\coverage.xml" -skipautoprops -filter:"+[Planner*]* -[Planner*]Planner.Data.Migrations* -[Planner.Test*]*"  -excludebyattribute:*.ExcludeFromCodeCoverage* -mergebyhash
+tools\OpenCover\tools\OpenCover.Console.exe -target:"tools\xunit.runner.console\tools\xunit.console.x86.exe" -targetargs:"Planner.Tests\bin\Debug\net462\Planner.Tests.exe -noShadow -appveyor" -register:user -output:"reports\coverage\coverage.xml" -skipautoprops -filter:"+[Planner*]* -[Planner*]Planner.Data.Migrations* -[Planner.Test*]*"  -excludebyattribute:*.ExcludeFromCodeCoverage* -mergebyhash
 
 if "%APPVEYOR%"=="True" tools\coveralls.io\tools\coveralls.net.exe --opencover reports\coverage\coverage.xml
 
