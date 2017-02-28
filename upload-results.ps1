c376cf5f2a261dd3f1dfc424047d3d6b18dc9175
Ilya Finkelshteyn <IlyaFinkelshteyn@users.noreply.github.com>
2017-02-28 14:16:38 -0800

    Adjust to local debug

--- upload-results.ps1
+++ upload-results.ps1
@@ -1,10 +1,10 @@
 # upload results to AppVeyor
 $wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\test-results.xml))
+$wc.UploadFile("http://appveyor.local/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\test-results.xml))
 
-Get-ChildItem ".\reports\karma-results\junit" -Filter *.xml | 
-Foreach-Object {
-	$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $_.FullName))
-}
+#Get-ChildItem ".\reports\karma-results\junit" -Filter *.xml | 
+#Foreach-Object {
+#	$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $_.FullName))
+#}
 
 
